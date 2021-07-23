3e671145ec291465b69a372eee8d4a2805d7f928
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-07-23 18:20:44 -0300

    ControladorDeRodadas.com

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,5 +1,17 @@
-
-
-
+const express = require('express');
+const app = express();
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+let i = 0;
+
+app.get("/rodadas", (req, res) => {
+    if (i < jogadores.length) {
+        res.send(`É a vez de ${jogadores[i]} jogar!`);
+        i++;
+    } else {
+        res.send('Próxima rodada!');
+        i = 0;
+    }
+});
+
+app.listen(8000);
