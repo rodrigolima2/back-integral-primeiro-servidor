f0d8178838d8fffee7d075372f76d345228c270a
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-07-23 20:26:59 -0300

    RoundControllerEvolution.com

--- extra-05/README.md
+++ extra-05/README.md
@@ -100,9 +100,9 @@ O índice informado (80) não existe no array. Novo jogador não adicionado.
 
 Preencha a checklist para finalizar o exercício:
 
-- [ ] Resolver o exercício
-- [ ] Adicionar as mudanças aos commits (`git add .` para adicionar todos os arquivos, ou `git add nome_do_arquivo` para adicionar um arquivo específico)
-- [ ] Commitar a cada mudança significativa ou na finalização do exercício (`git commit -m "Mensagem do commit"`)
-- [ ] Pushar os commits na sua branch na origem (`git push origin nome-da-branch`)
+- [x] Resolver o exercício
+- [x] Adicionar as mudanças aos commits (`git add .` para adicionar todos os arquivos, ou `git add nome_do_arquivo` para adicionar um arquivo específico)
+- [x] Commitar a cada mudança significativa ou na finalização do exercício (`git commit -m "Mensagem do commit"`)
+- [x] Pushar os commits na sua branch na origem (`git push origin nome-da-branch`)
 
 ###### tags: `nodeJS` `módulo 2` `exercício de casa` `primeiro servidor` `express` `http` `queryparam`
--- extra-05/index.js
+++ extra-05/index.js
@@ -1,5 +1,51 @@
-
-
-
+const express = require('express');
+const { send } = require('process');
+const app = express();
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+let contador = 0;
+
+app.get('/home', (req, res) => {
+    if (contador < jogadores.length) {
+        res.send(`É a vez de ${jogadores[contador]}`);
+        contador += 1;
+    } else {
+        res.send('Próxima rodada');
+        contador = 0;
+    }
+});
+app.get('/remover', (req, res) => {
+    if (req.query.indice !== undefined) {
+        const index = Number(req.query.indice);
+        if (index < jogadores.length) {
+            jogadores.splice(index, 1);
+            res.send(jogadores);
+        } else {
+            res.send(`Não existe jogador no índice informado (${index}) para ser removido.`);
+        }
+    } else {
+        res.send('Insira um valor válido');
+    }
+});
+app.get('/adicionar', (req, res) => {
+    if (req.query.nome !== undefined) {
+        let nome = req.query.nome;
+        nome = nome.substr(0, 1).toUpperCase() + nome.substr(1);
+        if (req.query.indice === undefined) {
+            jogadores.push(nome);
+            res.send(jogadores);
+        } else {
+            const indice = Number(req.query.indice);
+            if (indice < jogadores.length) {
+                jogadores.splice(indice, 0, nome);
+                res.send(jogadores);
+            } else {
+                res.send(`O índice informado (${indice}) não existe no array. Novo jogador não adicionado.`);
+            }
+        }
+    } else {
+        res.send('Insira um valor válido');
+    }
+});
+
+app.listen(8000);
